b9b6177913fdfeaed7c763496445c905d0acac71
zwasli <ladina99@gmail.com>
2015-11-16 23:13:59 +0100

    subjects.sql

--- subjects.sql
+++ subjects.sql
@@ -1,19 +1,41 @@
 -- This file is to fill the subject Table in the EF_Website_DB --
 USE EF_Website_Database;
 
-INSERT INTO table_name (name)
+Delete from subject;
+
+INSERT INTO subject (name)
 VALUES ("Englisch");
 
-INSERT INTO table_name (name)
-VALUES ("Englisch");
+INSERT INTO subject (name)
+VALUES ("Deutsch");
 
-INSERT INTO table_name (name)
-VALUES ("Englisch");
+INSERT INTO subject (name)
+VALUES ("Französisch");
 
-INSERT INTO table_name (name)
-VALUES ("Englisch");
-INSERT INTO table_name (name)
-VALUES ("Englisch");
+INSERT INTO subject (name)
+VALUES ("Algebra");
+
+INSERT INTO subject (name)
+VALUES ("Geometrie");
+
+INSERT INTO subject (name)
+VALUES ("Mathematik");
+
+INSERT INTO subject (name)
+VALUES ("Schwerpunktfach");
+
+INSERT INTO subject (name)
+VALUES ("Latein");
+
+INSERT INTO subject (name)
+VALUES ("Wirtschaft");
+
+INSERT INTO subject (name)
+VALUES ("Religonswissenschaft");
+
+
+
+
 
 
 
