a905ddb4781e29b1ab13be14607591d89df3b3ef
zwasli <ladina99@gmail.com>
2015-11-16 23:05:01 +0100

    subjects.sql

---
+++ subjects.sql
@@ -0,0 +1,20 @@
+-- This file is to fill the subject Table in the EF_Website_DB --
+USE EF_Website_Database;
+
+INSERT INTO table_name (name)
+VALUES ("Englisch");
+
+INSERT INTO table_name (name)
+VALUES ("Englisch");
+
+INSERT INTO table_name (name)
+VALUES ("Englisch");
+
+INSERT INTO table_name (name)
+VALUES ("Englisch");
+INSERT INTO table_name (name)
+VALUES ("Englisch");
+
+
+
+
