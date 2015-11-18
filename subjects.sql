924f99ada41f30c038984aebac35d06100863897
zwasli <ladina99@gmail.com>
2015-11-18 12:43:12 +0100

    subjects.sql

--- subjects.sql
+++ subjects.sql
@@ -4,34 +4,62 @@ USE EF_Website_Database;
 Delete from subject;
 
 INSERT INTO subject (name)
-VALUES ("Englisch");
-
-INSERT INTO subject (name)
 VALUES ("Deutsch");
 
 INSERT INTO subject (name)
-VALUES ("Franzoesisch");
+VALUES ("Französisch");
 
 INSERT INTO subject (name)
-VALUES ("Algebra");
-
-INSERT INTO subject (name)
-VALUES ("Geometrie");
+VALUES ("Englisch");
 
 INSERT INTO subject (name)
 VALUES ("Mathematik");
 
 INSERT INTO subject (name)
+VALUES ("Biologie");
+
+INSERT INTO subject (name)
+VALUES ("Chemie");
+
+INSERT INTO subject (name)
+VALUES ("Physik");
+
+INSERT INTO subject (name)
+VALUES ("Geschichte");
+
+INSERT INTO subject (name)
+VALUES ("Geographie");
+
+INSERT INTO subject (name)
+VALUES ("Bildnerisches Gestalten");
+
+INSERT INTO subject (name)
+VALUES ("Musik");
+
+INSERT INTO subject (name)
 VALUES ("Schwerpunktfach");
+
+INSERT INTO subject (name)
+VALUES ("Ergänzungsfach");
+
+INSERT INTO subject (name)
+VALUES ("Sport");
+
+INSERT INTO subject (name)
+VALUES ("Religionswissenschaft");
+
+INSERT INTO subject (name)
+VALUES ("Informatik");
 
 INSERT INTO subject (name)
 VALUES ("Latein");
 
 INSERT INTO subject (name)
-VALUES ("Wirtschaft");
+VALUES ("Geometrie");
 
 INSERT INTO subject (name)
-VALUES ("Religonswissenschaft");
+VALUES ("Algebra");
+
 
 
 
