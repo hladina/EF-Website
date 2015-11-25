d0204ebdfa96f324fbb2a7da2e19de3b2613c4be
zwasli <ladina99@gmail.com>
2015-11-25 14:16:49 +0100

    subjects.sql

--- dummyside.php
+++ dummyside.php
@@ -29,7 +29,7 @@ if($result === FALSE) {
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"].  "<br>";
+        echo ("id: " . $row["id"]. " - Name: " . $row["name"].  "<br>");
     }
 } else {
     echo "0 results";
